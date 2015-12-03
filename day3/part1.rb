9df51488d088bacd70080054009f95662e90d638
Piotr Brzózka <weemanjz@gmail.com>
2015-12-03 15:20:05 +0100

    shorten day3

--- day3/part1.rb
+++ day3/part1.rb
@@ -1,19 +1,13 @@
-x = 0
-y = 0
+pos = [0, 0]
+xy = { '<' => 0, '>' => 0, '^' => 1, 'v' => 1 }
+add = { '<' => -1, '>' => 1, '^' => 1, 'v' => -1 }
 city = { 0 => { 0 => true }}
 houses = 1
 
 gets.each_char do |c|
-  case c
-  when '^'
-    y += 1
-  when 'v'
-    y -= 1
-  when '<'
-    x -= 1
-  when '>'
-    x += 1
-  end
+  next if xy[c].nil?
+  pos[xy[c]] += add[c]
+  x, y = pos
   if city[x].nil? || city[x][y].nil?
     houses += 1
     city[x] ||= {}
--- day3/part2.rb
+++ day3/part2.rb
@@ -1,19 +1,13 @@
 pos = [[0, 0], [0, 0]]
+xy = { '<' => 0, '>' => 0, '^' => 1, 'v' => 1 }
+add = { '<' => -1, '>' => 1, '^' => 1, 'v' => -1 }
 city = { 0 => { 0 => true }}
 houses = 1
 
 gets.each_char.with_index do |c, i|
+  next if xy[c].nil?
   j = i % 2
-  case c
-  when '^'
-    pos[j][1] += 1
-  when 'v'
-    pos[j][1] -= 1
-  when '<'
-    pos[j][0] -= 1
-  when '>'
-    pos[j][0] += 1
-  end
+  pos[j][xy[c]] += add[c]
   x, y = pos[j]
   if city[x].nil? || city[x][y].nil?
     houses += 1
